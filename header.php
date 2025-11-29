3d590b2855ec0079dbdb13fb40555543ff684469
Kim Cyrus Tinio <kimcyrustinio@gmail.com>
2025-11-29 22:16:54 +0800

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Luxury Watch Store</title>
 </head>
 <body>
@@ -11,3 +11,4 @@
     <h1>Luxury Watch Store</h1>
     <hr>
 </header>
+
